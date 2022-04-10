68091586dd8dcf11a72e7d043fd4d23aaf03ac0a
Guilherme <guilhaugusto05@gmail.com>
2022-04-09 21:28:14 -0300

    Meio que adiciona layout responsivo na pagina da carteira

--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -6,17 +6,42 @@ import { Header, ExpenseForm, ExpensesTable } from '../components';
 import '../style/wallet-page.css';
 
 class Wallet extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      expanded: true,
+    };
+
+    this.handleExpand = this.handleExpand.bind(this);
+  }
+
+  handleExpand() {
+    this.setState((prev) => ({ expanded: !prev.expanded }));
+  }
+
   render() {
-    const { props: { isFetching, editing } } = this;
+    const { props: { isFetching, editing }, state: { expanded } } = this;
     const editingClass = editing ? 'column-editing' : '';
+
+    const leftColumnClass = expanded ? 'open' : 'close';
+    const expandBtnText = expanded ? 'Min.' : 'Max.';
 
     return (
       <div className="wallet-page d-flex">
         <div
           className={
-            `wallet-left-column d-flex flex-column align-items-center ${editingClass}`
+            `wallet-left-column d-flex flex-column align-items-center
+            ${editingClass} ${leftColumnClass}`
           }
         >
+          <button
+            type="button"
+            className="expand-btn"
+            onClick={ this.handleExpand }
+          >
+            {expandBtnText}
+          </button>
           <Header />
           <div className="line" />
           <ExpenseForm />
